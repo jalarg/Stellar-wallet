5db3f02d5b98409d70f017322ba46e0c9fff2162
jalarg <jlema1990@gmail.com>
2023-10-02 15:21:38 -0300

    Add stellar icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+"use client";
+import Image from "next/image";
+import logo from "./assets/stellar-xlm-logo.svg";
+
 export default function Home() {
-  return <h1 className="font-bold">Stellar wallet!</h1>;
+  return (
+    <div className="relative h-[100vh]">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md z-30">
+        <Image
+          src={logo}
+          width={100}
+          height={100}
+          alt="logo"
+          className="mx-auto w-auto h-20 rounded-lg cursor-pointer hover:scale-110 pt-5"
+        />
+      </div>
+    </div>
+  );
 }
