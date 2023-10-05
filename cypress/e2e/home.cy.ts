f0a74323da3f19ea9b248e1159579dd2625abb9d
jalarg <jlema1990@gmail.com>
2023-10-05 10:16:22 -0300

    Refactor tests for home and navbar

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -5,10 +5,11 @@ beforeEach(() => {
 });
 
 describe("Home: UI and functionality:", () => {
-  it("Home UI: Stellar logo", () => {
+  it("Should the Stellar logo be visible", () => {
     cy.get('[data-cy="stellar-home-logo"]').should("be.visible");
   });
-  it("Home UI: Should show a title and two paragraphs", () => {
+
+  it("Should the title and two paragraphs be visible and contain the text description", () => {
     cy.get('[data-cy="rocket-title"]')
       .should("be.visible")
       .and("contain", "Rocket");
@@ -20,48 +21,54 @@ describe("Home: UI and functionality:", () => {
       .and("contain", "Sign in methods");
   });
 
-  it("Button connect: Should show a button to generate connect", () => {
-    cy.get('[data-cy="connect"]')
+  it("Should show a button to generate connect", () => {
+    cy.get('[data-cy="connect"]').as("connectButton");
+
+    cy.get("@connectButton")
+      .should("exist")
       .should("be.visible")
       .and("contain", "Connect with a secret key");
-    cy.get('[data-cy="connect"]').should("exist");
   });
 
-  it("Button create wallet: Should show a button to generate a pair of new keys", () => {
-    cy.get('[data-cy="register"]').should("exist");
-    cy.get('[data-cy="register"]').should(
-      "have.text",
-      "Generate key pair for a new account"
-    );
-    cy.get('[data-cy="public-key"]').should("not.exist");
-    cy.get('[data-cy="private-key"]').should("not.exist");
-    cy.get('[data-cy="confirm-wallet"]').should("not.exist");
-    cy.get('[data-cy="go-back"]').should("not.exist");
-    cy.get('[data-cy="register"]').click();
-    cy.get('[data-cy="public-key"]').should("exist");
-    cy.get('[data-cy="private-key"]').should("exist");
-    cy.get('[data-cy="confirm-wallet"]').should("exist");
-    cy.get('[data-cy="go-back"]').should("exist");
-    cy.get('[data-cy="confirm-wallet"]').should("have.text", "Confirm wallet");
-    cy.get('[data-cy="go-back"]').should("have.text", "Go back");
+  it("Should show a button to generate a pair of new keys and should  have create public and private key functionality", () => {
+    cy.get('[data-cy="register"]').as("registerButton");
+    cy.get("@registerButton")
+      .should("exist")
+      .and("have.text", "Generate key pair for a new account");
+    cy.get("@registerButton").click();
+    cy.get('[data-cy="private-key"]').as("privateKeyInput");
+    cy.get('[data-cy="public-key"]').as("publicKeyInput");
+    cy.get('[data-cy="confirm-wallet"]').as("confirmWalletButton");
+    cy.get('[data-cy="go-back"]').as("goBackButton");
+    cy.get("@publicKeyInput").should("exist");
+    cy.get("@privateKeyInput").should("exist");
+    cy.get("@confirmWalletButton").should("exist");
+    cy.get("@goBackButton").should("exist");
+    cy.get("@goBackButton").click();
+    cy.get("@publicKeyInput").should("not.exist");
+    cy.get("@privateKeyInput").should("not.exist");
+    cy.get("@publicKeyInput").should("not.exist");
+    cy.get("@privateKeyInput").should("not.exist");
   });
 });
-
 describe(" Navbar: UI and functionality:", () => {
-  it("Check Stellar Icon functionality", () => {
-    // cy.visit(projectUrl);
+  it("Should the navbar container be visible", () => {
+    cy.get('[data-cy="navbar-container"]').should("exist").and("be.visible");
+  });
+
+  it("Should the Stellar logo be visible and have a href", () => {
     cy.get('[data-cy="navbar-container"]').should("exist").and("be.visible");
     cy.get('[data-cy="stellar-icon"]')
       .should("be.visible")
       .and("have.attr", "href", stellarIconUrl);
   });
 
-  it("Check theme functionality", () => {
-    // cy.visit(projectUrl);
-    cy.get('[data-cy="theme-button-light"]').should("exist");
+  it("Should the theme icon exist and have the toggle theme functionality", () => {
+    cy.get('[data-cy="theme-button-light"]').as("themeButtonLight");
+    cy.get("@themeButtonLight").should("exist");
     cy.get('[data-cy="theme-button-dark"]').should("not.exist");
-    cy.get('[data-cy="theme-button-light"]').click();
-    cy.get('[data-cy="theme-button-light"]').should("not.exist");
+    cy.get("@themeButtonLight").click().should("not.exist");
     cy.get('[data-cy="theme-button-dark"]').should("exist");
+    cy.get("@themeButtonLight").should("not.exist");
   });
 });
