890581b8a9df55bc2cb3e7ea870a873cbb9043fe
jalarg <jlema1990@gmail.com>
2023-10-05 17:25:07 -0300

    Add modal data folder

---
+++ src/data/modals.ts
@@ -0,0 +1,9 @@
+export const generateKeyPairData = {
+    title: "Generate a new keypair",
+    subtitle: "ATTENTION: Secret key wallets are not safe:",
+    list: [
+      "Pasting your secret key makes you vulnerable to accidents, attacks, and scams that can result in the loss of funds.",
+      "It is safer to create an account using methods that do not share your secret key with websites, such as hardware wallets or browser extensions.",
+    ],
+  };
+
