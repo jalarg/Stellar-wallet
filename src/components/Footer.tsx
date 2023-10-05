52b73645acb420de25686885d5590e1950a77677
jalarg <jlema1990@gmail.com>
2023-10-05 15:50:02 -0300

    Add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="footer-container bg-neutralMain flex flex-col px-[12.5%] text-black">
+      <div className="border-t-2 border-black" />
+    </footer>
+  );
+};
+
+export default Footer;
