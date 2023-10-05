f17feb9a58fd9da769b79065cb550e5cdb7274f9
jalarg <jlema1990@gmail.com>
2023-10-05 08:48:47 -0300

    Add testing for home logo, titles and paragraph

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,8 +1,22 @@
-const projectUrl = Cypress.env("BASE_URL");
+import { projectUrl } from "../support/constants";
 
-describe("Navigation", () => {
-  it("Go to the website:", () => {
-    console.log(projectUrl, "ProjectURL");
-    cy.visit(projectUrl);
+beforeEach(() => {
+  cy.visit(projectUrl);
+});
+
+describe("Home: UI and functionality:", () => {
+  it("Home UI: Stellar logo", () => {
+    cy.get('[data-cy="stellar-home-logo"]').should("be.visible");
+  });
+  it("Home UI: Should show a title and two paragraphs", () => {
+    cy.get('[data-cy="rocket-title"]')
+      .should("be.visible")
+      .and("contain", "Rocket");
+    cy.get('[data-cy="rocket-description-1"]')
+      .should("be.visible")
+      .and("contain", "The easiest way to connect with a wallet");
+    cy.get('[data-cy="rocket-description-2"]')
+      .should("be.visible")
+      .and("contain", "Sign in methods");
   });
 });
