c9df2780d6482291602c2c6dce1152fb7de7bbb5
jalarg <jlema1990@gmail.com>
2023-10-05 16:05:21 -0300

    Add index with footer and footer including href to github

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
+import Image from "next/image";
+import logo from "../assets/github.svg";
+
 const Footer = () => {
   return (
     <footer className="footer-container bg-neutralMain flex flex-col px-[12.5%] text-black">
       <div className="border-t-2 border-black" />
+      <div className="flex flex-col lg:flex-row py-5 items-center justify-between">
+        <div className="flex justify-center lg:justify-start gap-5 lg:gap-20 hover:font-bold">
+          <Link
+            className="github-icon"
+            href="https://github.com/jalarg/Stellar-wallet"
+          >
+            <Image src={logo} alt="github" width={50} height={50} />
+          </Link>
+        </div>
+      </div>
     </footer>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,87 +6,89 @@ import Button from "../components/commons/Button";
 import { useState } from "react";
 import Register from "../components/Register";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export default function Page() {
   const [secret, setSecret] = useState<string | null>(null);
   const [publicKey, setPublicKey] = useState<string | null>(null);
 
   return (
-    <div className="relative h-[100vh]">
-      <Navbar />
-      <div className="sm:mx-auto sm:w-full sm:max-w-md z-30">
-        <Image      
-          src={logo}
-          width={100}
-          height={100}
-          alt="logo"
-          className="stellar-home-logo mx-auto w-auto h-20 rounded-lg cursor-pointer hover:scale-110 pt-5"
-        />
-      </div>
-      <div className="border-2 rounded-[25px] my-6 mx-auto w-[90%] sm:max-w-lg h-900px] z-30 bg-gray-100 p-3 flex flex-col">
-        <h1         
-          className="rocket-title mb-8 text-center text-3xl font-bold tracking-tight text-gray-800"
-        >
-          Rocket
-        </h1>
-        <p    
-          className="rocket-description-1 mb-8 text-center text-xl font-bold tracking-tight text-gray-800"
-        >
-          The easiest way to connect with a wallet
-        </p>
-        <p
-          className="rocket-description-2 mb-8 text-center text-xl font-bold tracking-tight text-gray-800"
-        >
-          Sign in methods
-        </p>
+    <>
+      <div className="relative h-[80vh]">
+        <Navbar />
+        <div className="sm:mx-auto sm:w-full sm:max-w-md z-30">
+          <Image
+            src={logo}
+            width={100}
+            height={100}
+            alt="logo"
+            className="stellar-home-logo mx-auto w-auto h-20 rounded-lg cursor-pointer hover:scale-110 pt-5"
+          />
+        </div>
+        <div className="border-2 rounded-[25px] my-6 mx-auto w-[90%] sm:max-w-lg h-900px] z-30 bg-gray-100 p-3 flex flex-col">
+          <h1 className="rocket-title mb-8 text-center text-3xl font-bold tracking-tight text-gray-800">
+            Rocket
+          </h1>
+          <p className="rocket-description-1 mb-8 text-center text-xl font-bold tracking-tight text-gray-800">
+            The easiest way to connect with a wallet
+          </p>
+          <p className="rocket-description-2 mb-8 text-center text-xl font-bold tracking-tight text-gray-800">
+            Sign in methods
+          </p>
 
-        <div className="flex flex-col justify-center m-5 px-5 ">
-          {secret && (
-            <div className="flex flex-col justify-center px-5 ">
-              <Input
-                label="Secret key"
-                value={secret}
-                readOnly={true}
-                required={true}
-              />
-              <Input
-                label="Public key"
-                value={publicKey}
-                readOnly={true}
-                required={true}
-              />
-              <div className="flex flex-col justify-center m-5">
-                <Button buttonClass="button-confirm-wallet" secondary fullWidth>
-                  Confirm wallet
-                </Button>
+          <div className="flex flex-col justify-center m-5 px-5 ">
+            {secret && (
+              <div className="flex flex-col justify-center px-5 ">
+                <Input
+                  label="Secret key"
+                  value={secret}
+                  readOnly={true}
+                  required={true}
+                />
+                <Input
+                  label="Public key"
+                  value={publicKey}
+                  readOnly={true}
+                  required={true}
+                />
+                <div className="flex flex-col justify-center m-5">
+                  <Button
+                    buttonClass="button-confirm-wallet"
+                    secondary
+                    fullWidth
+                  >
+                    Confirm wallet
+                  </Button>
+                </div>
+                <div className="flex flex-col justify-center m-5">
+                  <Button
+                    buttonClass="button-go-back"
+                    onClick={() => {
+                      setPublicKey(null);
+                      setSecret(null);
+                    }}
+                    fullWidth
+                    danger
+                  >
+                    Go back
+                  </Button>
+                </div>
               </div>
-              <div className="flex flex-col justify-center m-5">
-                <Button
-                  buttonClass="button-go-back"
-                  onClick={() => {
-                    setPublicKey(null);
-                    setSecret(null);
-                  }}
-                  fullWidth
-                  danger
-                >
-                  Go back
-                </Button>
+            )}
+            {!secret && (
+              <div className="flex flex-col justify-center m-5 px-5">
+                <div className="flex flex-col justify-center m-5">
+                  <Button buttonClass="button-connect" fullWidth>
+                    Connect with a secret key
+                  </Button>
+                </div>
+                <Register setSecret={setSecret} setPublicKey={setPublicKey} />
               </div>
-            </div>
-          )}
-          {!secret && (
-            <div className="flex flex-col justify-center m-5 px-5">
-              <div className="flex flex-col justify-center m-5">
-                <Button buttonClass="button-connect" fullWidth>
-                  Connect with a secret key
-                </Button>
-              </div>
-              <Register setSecret={setSecret} setPublicKey={setPublicKey} />
-            </div>
-          )}
+            )}
+          </div>
         </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
