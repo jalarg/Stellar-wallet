637d13610544bb2c96c3c1c5dec2c6cb970aa340
jalarg <jlema1990@gmail.com>
2023-10-05 08:50:07 -0300

    Add test for buttons functionality

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -19,4 +19,30 @@ describe("Home: UI and functionality:", () => {
       .should("be.visible")
       .and("contain", "Sign in methods");
   });
+
+  it("Button connect: Should show a button to generate connect", () => {
+    cy.get('[data-cy="connect"]')
+      .should("be.visible")
+      .and("contain", "Connect with a secret key");
+    cy.get('[data-cy="connect"]').should("exist");
+  });
+
+  it("Button create wallet: Should show a button to generate a pair of new keys", () => {
+    cy.get('[data-cy="register"]').should("exist");
+    cy.get('[data-cy="register"]').should(
+      "have.text",
+      "Generate key pair for a new account"
+    );
+    cy.get('[data-cy="public-key"]').should("not.exist");
+    cy.get('[data-cy="private-key"]').should("not.exist");
+    cy.get('[data-cy="confirm-wallet"]').should("not.exist");
+    cy.get('[data-cy="go-back"]').should("not.exist");
+    cy.get('[data-cy="register"]').click();
+    cy.get('[data-cy="public-key"]').should("exist");
+    cy.get('[data-cy="private-key"]').should("exist");
+    cy.get('[data-cy="confirm-wallet"]').should("exist");
+    cy.get('[data-cy="go-back"]').should("exist");
+    cy.get('[data-cy="confirm-wallet"]').should("have.text", "Confirm wallet");
+    cy.get('[data-cy="go-back"]').should("have.text", "Go back");
+  });
 });
