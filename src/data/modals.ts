7b4f7c604fa6ac893b951a907c485799422e34f1
jalarg <jlema1990@gmail.com>
2023-10-06 08:36:12 -0300

    Add custom modal-step2 data

--- src/data/modals.ts
+++ src/data/modals.ts
@@ -1,9 +1,21 @@
-export const generateKeyPairData = {
-    title: "Generate a new keypair",
-    subtitle: "ATTENTION: Secret key wallets are not safe:",
-    list: [
-      "Pasting your secret key makes you vulnerable to accidents, attacks, and scams that can result in the loss of funds.",
-      "It is safer to create an account using methods that do not share your secret key with websites, such as hardware wallets or browser extensions.",
-    ],
-  };
+export const generateKeyPairDataStep1 = {
+  title: "Generate a new keypair",
+  subtitle: "ATTENTION: Secret key wallets are not safe:",
+  list: [
+    "Pasting your secret key makes you vulnerable to accidents, attacks, and scams that can result in the loss of funds.",
+    "It is safer to create an account using methods that do not share your secret key with websites, such as hardware wallets or browser extensions.",
+  ],
+};
 
+export const generateKeyPairDataStep2 = {
+  title: "Generate a new keypair",
+  subtitle: "ATTENTION:",
+  list: [
+    "It is very important to save your secret key and store it somewhere safe.",
+    "If you lose it, you will lose access to your account. No one in the known universe will be able to help you get back in.",
+    "SDF does not store a copy of your keys and cannot help you recover lost keys.",
+    "Anyone who knows your secret key has access to your funds.",
+    "You have several options: Write your key down on a piece of paper. Keep it in a safe. Store it in a password manager. Use a hardware wallet. But don't ever keep it unencrypted on your computer or in your email.",
+    "Note: Connecting by entering a secret key may be deprecated in a future version of the Account Viewer.",
+  ],
+};
