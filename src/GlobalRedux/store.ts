72f3852da52077159368408172eb6fbca2e2d002
jalarg <jlema1990@gmail.com>
2023-10-10 14:25:25 -0300

    Add store de redux

---
+++ src/GlobalRedux/store.ts
@@ -0,0 +1,16 @@
+"use client";
+
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./Features/authSlice";
+import { login, logout } from "./Features/authSlice";
+
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export { login, logout };
