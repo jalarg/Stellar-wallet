e81a4cdf914804158525b3ddb4e91214276bf232
jalarg <jlema1990@gmail.com>
2023-10-11 15:21:34 -0300

    Add index file for functions exports

---
+++ src/actions/index.ts
@@ -0,0 +1,11 @@
+import minimumBalance from "./minimumBalance";
+import checkBalance from "./checkBalance";
+import sendTransaction from "./sendTransaction";
+import setKeypair from "./setKeypair";
+
+export {
+  minimumBalance,
+  checkBalance,
+  sendTransaction,
+  setKeypair,
+};
