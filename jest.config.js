54a6075c1f59aae696778074b621fd1d63a4a4d4
jalarg <jlema1990@gmail.com>
2023-10-14 12:45:19 -0300

    Add jest.config

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,8 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["**/__tests__/**/*.spec.ts"],
+  transform: {
+    "^.+\\.ts$": "ts-jest",
+  },
+  moduleFileExtensions: ["ts", "js", "json", "node"],
 };
