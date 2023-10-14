9c5e450450e918a7c679228634556d57b9e18929
jalarg <jlema1990@gmail.com>
2023-10-14 12:56:23 -0300

    Add initial tests for setKeypairs

---
+++ jest/__tests__/setKeyPairs.spec.ts
@@ -0,0 +1,26 @@
+import "jest";
+import { expect } from "@jest/globals";
+import setKeypair from "../../src/actions/setKeypair";
+import { Keypair } from "stellar-sdk";
+
+describe("setKeypair function", () => {
+  it("Should set secret and public key when given a key pair", () => {
+    const setSecret = jest.fn();
+    const setPublicKey = jest.fn();
+    const realPair = Keypair.random();
+
+    setKeypair({ setSecret, setPublicKey, pair: realPair });
+
+    expect(setSecret).toHaveBeenCalledWith(realPair.secret());
+    expect(setPublicKey).toHaveBeenCalledWith(realPair.publicKey());
+  });
+
+  it("Should set null for secret and public key when not given a key pair", () => {
+    const setSecret = jest.fn();
+    const setPublicKey = jest.fn();
+    setKeypair({ setSecret, setPublicKey, pair: undefined });
+
+    expect(setSecret).toHaveBeenCalledWith(null);
+    expect(setPublicKey).toHaveBeenCalledWith(null);
+  });
+});
