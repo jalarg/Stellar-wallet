07e279e9ba0af594265a7ccc5c5dcb2c47263ea7
jalarg <jlema1990@gmail.com>
2023-10-17 18:04:58 -0300

    Refactor minimumBalance function

--- src/actions/minimumBalance.ts
+++ src/actions/minimumBalance.ts
@@ -1,16 +1,24 @@
-async function minimumBalance(publicKey: string) {
-  try {
-    if (publicKey) {
-      const response = await fetch(
-        `https://friendbot.stellar.org?addr=${encodeURIComponent(publicKey)}`
-      );
-      const responseJSON = await response.json();
+interface ResponseData {
+  status: number;
+  detail: string;
+  error: string;
+}
 
-      if (responseJSON.status === 400) {
-        console.error(`ERROR ${responseJSON.detail} !`, responseJSON.error);
-      } else {
-        console.log("SUCCESS! You have a new account :)\n", responseJSON);
-      }
+async function minimumBalance(publicKey: string | null) {
+  try {
+    if (!publicKey) {
+      throw new Error("Public key is null or empty");
+    }
+    const response = await fetch(
+      `https://friendbot.stellar.org?addr=${encodeURIComponent(publicKey)}`
+    );
+
+    const responseJSON = (await response.json()) as ResponseData;
+    
+    if (responseJSON.status === 400) {
+      console.error(`ERROR ${responseJSON.detail} !`, responseJSON.error);
+    } else {
+      console.log("SUCCESS! You have a new account :)\n", responseJSON);
     }
   } catch (err) {
     console.error("ERROR!", err);
