572edaf71235d66e471bb07adfe6bd57652da030
jalarg <jlema1990@gmail.com>
2023-10-18 12:58:38 -0300

    Refactor wallet modals with switch functionality

--- src/components/modals/wallet/index.tsx
+++ src/components/modals/wallet/index.tsx
@@ -1,4 +1,41 @@
 import ReceiveModal from "./ReceiveModal";
 import SendModal from "./SendModal";
+import { IModalWallet } from "../../../types/types";
+import { sendTransaction, receiveTransaction } from "../../../content/modals";
 
-export { ReceiveModal, SendModal };
+const Modal: React.FC<IModalWallet> = ({
+  activeModal,
+  isOpen,
+  onClose,
+  label,
+  secretKey,
+  publicKey,
+}) => {
+  switch (activeModal) {
+    case "ReceiveTransaction":
+      return (
+        <ReceiveModal
+          label={label}
+          isOpen={isOpen}
+          onClose={onClose}
+          content={receiveTransaction}
+          publicKey={publicKey}
+        />
+      );
+    case "SendTransaction":
+      return (
+        <SendModal
+          label={label}
+          isOpen={isOpen}
+          onClose={onClose}
+          content={sendTransaction}
+          secretKey={secretKey}
+          publicKey={publicKey}
+        />
+      );
+    default:
+      return null;
+  }
+};
+
+export default Modal;
