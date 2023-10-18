f8011094130fb3691096db05f0a2275227d7a541
jalarg <jlema1990@gmail.com>
2023-10-18 17:24:21 -0300

    Refactor paymentsHistory function

--- src/actions/paymentsHistory.ts
+++ src/actions/paymentsHistory.ts
@@ -9,36 +9,41 @@ interface IPayment {
 async function paymentsHistory(publicKey: string): Promise<IPayment[]> {
   return new Promise<IPayment[]>(async (resolve, reject) => {
     try {
+      if (!publicKey) {
+        throw new Error("Public key is null or empty");
+      }
       const paymentData: IPayment[] = [];
-      const payments = server.payments().forAccount(publicKey);
-      payments.stream({
-        onmessage: function (payment: any) {
-          if (payment.to !== publicKey) {
-            return;
-          }
-          var asset;
-          if (payment.asset_type === "native") {
-            asset = "lumens";
-          } else {
-            asset = payment.asset_code + ":" + payment.asset_issuer;
-          }
+      server
+        .payments()
+        .forAccount(publicKey)
+        .stream({
+          onmessage: function (payment: any) {
+            if (payment.to !== publicKey) {
+              return;
+            }
+            var asset;
+            if (payment.asset_type === "native") {
+              asset = "lumens";
+            } else {
+              asset = payment.asset_code + ":" + payment.asset_issuer;
+            }
 
-          const paymentEntry: IPayment = {
-            amount: payment.amount,
-            asset: asset,
-            sender: payment.from,
-          };
+            const paymentEntry: IPayment = {
+              amount: payment.amount,
+              asset: asset,
+              sender: payment.from,
+            };
 
-          if (payment.type !== "create_account") {
-            paymentData.push(paymentEntry);
-          }
-        },
-        onerror: function (error: any) {
-          console.error("Error in payment stream");
-          reject(error);
-        },
-      });
-      console.log("payments", paymentData);
+            if (payment.type !== "create_account") {
+              paymentData.push(paymentEntry);
+            }
+          },
+          onerror: function (error: any) {
+            console.error("Error in payment stream");
+            reject(error);
+          },
+        });
+      resolve(paymentData);
     } catch (err) {
       console.error("ERROR!", err);
       reject(err);
