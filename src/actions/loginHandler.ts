af7f2c4bd7c83d98bf98770fc6b884dae62a6809
jalarg <jlema1990@gmail.com>
2023-10-20 16:01:12 -0300

    Add login handler

---
+++ src/actions/loginHandler.ts
@@ -0,0 +1,33 @@
+import { IAuthState } from "../types/types";
+import { login } from "../GlobalRedux/store";
+import { Keypair } from "stellar-sdk";
+import { message } from "antd";
+import { ILogin } from "../types/types";
+
+async function loginHandler({ publicKey, secretKey, dispatch, auth }: ILogin) {
+  try {
+    if (auth.isAuthenticated) {
+      message.error("You are already logged in!");
+      dispatch({
+        type: "AUTH_ERROR",
+        payload: {
+          error: "You are already logged in!",
+        },
+      });
+      message.error("You are already logged in!");
+      return;
+    }
+    if (!publicKey) {
+      publicKey = Keypair.fromSecret(secretKey).publicKey();
+      dispatch(login({ publicKey, secretKey }));
+      message.success("Login successful!");
+      return;
+    }
+    dispatch(login({ publicKey, secretKey }));
+    message.success("Login successful!");
+  } catch (error) {
+    console.log("Error:", error);
+  }
+}
+
+export default loginHandler;
