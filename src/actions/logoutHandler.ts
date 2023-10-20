df77c3058b81124488cf4d34c8931b8fc7322689
jalarg <jlema1990@gmail.com>
2023-10-20 16:08:30 -0300

    Add handler logout

---
+++ src/actions/logoutHandler.ts
@@ -0,0 +1,13 @@
+import { logout } from "../GlobalRedux/store";
+import { message } from "antd";
+
+async function logoutHandler(dispatch: any) {
+  try {
+    dispatch(logout());
+    message.success("Logout Successful");
+  } catch (error) {
+    console.log("Error:", error);
+  }
+}
+
+export default logoutHandler;
