447b7b04bedd0fb36562480e24f6a2ddcde2eb79
jalarg <jlema1990@gmail.com>
2023-10-23 15:46:09 -0300

    Refactor isAuth to use handleWalletInformation

--- src/components/withAuth.tsx
+++ src/components/withAuth.tsx
@@ -1,29 +1,54 @@
 import { useSelector } from "react-redux";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { checkBalance, paymentsHistory } from "../actions/stellar";
+import { Spiral } from "./commons";
+import { IAuth } from "../types/types";
+import { handleWalletInformation } from "../actions/handlers";
 
 export default function withAuth(WrappedComponent: any) {
   return function WithAuthWrapper(props: any) {
-    const { isAuthenticated } = useSelector((state: any) => state.auth);
+    const { isAuthenticated } = useSelector((state: IAuth) => state.auth);
     const publicKey = useSelector(
-      (state: any) => state.auth.walletCredentials.publicKey
+      (state: IAuth) => state.auth.walletCredentials.publicKey
     );
     const secretKey = useSelector(
-      (state: any) => state.auth.walletCredentials.secretKey
+      (state: IAuth) => state.auth.walletCredentials.secretKey
     );
+    const [isLoading, setIsLoading] = useState(false);
+    const [balance, setBalance] = useState("0");
+    const [payments, setPayments] = useState(null);
     const router = useRouter();
+
     useEffect(() => {
       if (!isAuthenticated) {
         router.push("/");
       }
+      handleWalletInformation({
+        publicKey,
+        setBalance,
+        setPayments,
+        setIsLoading,
+      });
     }, [isAuthenticated]);
 
     return (
-      <WrappedComponent
-        {...props}
-        publicKey={publicKey}
-        secretKey={secretKey}
-      />
+      <>
+        {isLoading ? (
+          <div className="flex justify-center items-center h-screen">
+            <Spiral size="large" />
+          </div>
+        ) : (
+          <WrappedComponent
+            {...props}
+            publicKey={publicKey}
+            secretKey={secretKey}
+            balance={balance}
+            payments={payments}
+            setBalance={setBalance}
+          />
+        )}
+      </>
     );
   };
 }
