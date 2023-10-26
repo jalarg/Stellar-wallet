21d828bff0840a36e557b1cca7e16d49a7c24483
jalarg <jlema1990@gmail.com>
2023-10-26 06:08:36 -0300

    Add albedo wallet

---
+++ src/actions/wallets/albedoWallet.ts
@@ -0,0 +1,33 @@
+import PrivateKeyWallet from "./privateKeyWallet";
+import albedoLogin from "../stellar/albedoLogin";
+import albedo from "@albedo-link/intent";
+
+interface IWallet {
+  publicKey: string;
+  secretKey: string;
+}
+
+interface IAlbedo {
+  destinationId: string;
+  amount: string;
+}
+
+export default class Albedo extends PrivateKeyWallet {
+  constructor({ publicKey, secretKey }: IWallet) {
+    super({ publicKey, secretKey });
+  }
+
+  async login(dispatch: any) {
+    albedoLogin(dispatch);
+  }
+
+  async sendTransaction({ destinationId, amount }: IAlbedo): Promise<any> {
+    const result = await albedo.pay({
+      destination: destinationId,
+      amount,
+      network: "testnet",
+      submit: true,
+    });
+    return result;
+  }
+}
