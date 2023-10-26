60581e9eb5d3d3828f5f6f5519a24c2f23af4f81
jalarg <jlema1990@gmail.com>
2023-10-26 18:41:20 -0300

    Remove function from wallet class

--- src/actions/wallets/privateKeyWallet.ts
+++ src/actions/wallets/privateKeyWallet.ts
@@ -10,13 +10,6 @@ interface IWallet {
   secretKey: string;
 }
 
-interface IPayment {
-  amount: string;
-  asset: string;
-  sender: string;
-  receiver: string;
-}
-
 export default class PrivateKeyWallet {
   publicKey: string;
   secretKey: string;
@@ -24,67 +17,6 @@ export default class PrivateKeyWallet {
   constructor({ publicKey, secretKey }: IWallet) {
     this.publicKey = publicKey;
     this.secretKey = secretKey;
-  }
-
-  async checkBalance() {
-    try {
-      const accountBalance = await server.loadAccount(this.publicKey);
-      const balances = accountBalance.balances.map(
-        ({ asset_type, balance }: IBalance) => ({
-          asset_type,
-          balance,
-        })
-      );
-      return balances;
-    } catch (err: any) {
-      if (!isValidPublicKey(this.publicKey)) {
-        throw new Error("Please provide a public key in the correct format.");
-      }
-      throw new Error("Account not found. Please check the public key.");
-    }
-  }
-
-  async getPaymentsHistory(): Promise<IPayment[]> {
-    return new Promise<IPayment[]>(async (resolve, reject) => {
-      try {
-        if (!this.publicKey) {
-          throw new Error("Public key is null or empty");
-        }
-        const paymentData: IPayment[] = [];
-        server
-          .payments()
-          .forAccount(this.publicKey)
-          .stream({
-            onmessage: (payment: any) => {
-              var asset;
-              if (payment.asset_type === "native") {
-                asset = "lumens";
-              } else {
-                asset = payment.asset_code + ":" + payment.asset_issuer;
-              }
-
-              const paymentEntry: IPayment = {
-                amount: payment.amount,
-                asset: asset,
-                sender: payment.from,
-                receiver: payment.to,
-              };
-
-              if (payment.type !== "create_account") {
-                paymentData.push(paymentEntry);
-                resolve(paymentData);
-              }
-            },
-            onerror: (error: any) => {
-              console.error("Error in payment stream");
-              reject(error);
-            },
-          });
-      } catch (err) {
-        console.error("ERROR!", err);
-        reject(err);
-      }
-    });
   }
 
   async sendTransaction({ destinationId, amount }: ISendTransactionFunction) {
