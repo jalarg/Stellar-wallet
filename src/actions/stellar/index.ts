fbb5912358fb97ebf0c9dbf1f993ca35d8777639
jalarg <jlema1990@gmail.com>
2023-10-26 09:55:56 -0300

    Remove functions from index

--- src/actions/stellar/index.ts
+++ src/actions/stellar/index.ts
@@ -1,18 +1,12 @@
 import addMinimumBalance from "./addMinimumBalance";
-import checkBalance from "./checkBalance";
-import sendTransaction from "./sendTransaction";
 import setKeypair from "./setKeypair";
-import getPaymentsHistory from "./getPaymentsHistory";
 import server from "./server";
 import resetKeys from "./resetKeys";
 import albedoLogin from "./albedoLogin";
 
 export {
   addMinimumBalance,
-  checkBalance,
-  sendTransaction,
   setKeypair,
-  getPaymentsHistory,
   server,
   resetKeys,
   albedoLogin,
