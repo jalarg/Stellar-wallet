6ff8775459245b10c84b089a5818c83c73a7bca9
jalarg <jlema1990@gmail.com>
2023-10-26 06:45:31 -0300

    Add checkbalance to walletPrivateKey

--- src/actions/wallets/privateKeyWallet.ts
+++ src/actions/wallets/privateKeyWallet.ts
@@ -1,9 +1,9 @@
 import { ISendTransactionFunction } from "../../types/types";
-import {
-  checkBalance,
-  getPaymentsHistory,
-  sendTransaction,
-} from "../stellar";
+import { IBalance } from "../../types/types";
+import isValidPublicKey from "../../validations/isValidPublicKey";
+import { server } from "../stellar";
+
+import { getPaymentsHistory, sendTransaction } from "../stellar";
 
 interface IWallet {
   publicKey: string;
@@ -14,28 +14,35 @@ export default class PrivateKeyWallet {
   publicKey: string;
   secretKey: string;
 
-  constructor({
-    publicKey,
-    secretKey,  
-  }: IWallet) {
+  constructor({ publicKey, secretKey }: IWallet) {
     this.publicKey = publicKey;
-    this.secretKey = secretKey;  
+    this.secretKey = secretKey;
   }
-  
+
   async checkBalance() {
-    const balance = await checkBalance(this.publicKey);
-    return balance;
+    try {
+      const accountBalance = await server.loadAccount(this.publicKey);
+      const balances = accountBalance.balances.map(
+        ({ asset_type, balance }: IBalance) => ({
+          asset_type,
+          balance,
+        })
+      );
+      return balances;
+    } catch (err: any) {
+      if (!isValidPublicKey(this.publicKey)) {
+        throw new Error("Please provide a public key in the correct format.");
+      }
+      throw new Error("Account not found. Please check the public key.");
+    }
   }
 
   async fetchPaymentsHistory() {
     const paymentsHistory = await getPaymentsHistory(this.publicKey);
     return paymentsHistory;
-  }  
+  }
 
-  async sendTransaction({
-    destinationId,
-    amount,
-  }: ISendTransactionFunction) {
+  async sendTransaction({ destinationId, amount }: ISendTransactionFunction) {
     const result = await sendTransaction({
       publicKey: this.publicKey,
       privateKey: this.secretKey,
@@ -43,5 +50,5 @@ export default class PrivateKeyWallet {
       amount,
     });
     return result;
-  }  
+  }
 }
