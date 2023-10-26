7a9a98ac42547fb429bd6620094dc0cce76c3726
jalarg <jlema1990@gmail.com>
2023-10-26 18:38:42 -0300

    Move functions from wallet to stellar folder

---
+++ src/actions/stellar/getPaymentsHistory.ts
@@ -0,0 +1,53 @@
+import { server } from ".";
+
+interface IPayment {
+  amount: string;
+  asset: string;
+  sender: string;
+  receiver: string;
+}
+
+async function paymentsHistory(publicKey: string): Promise<IPayment[]> {
+  return new Promise<IPayment[]>(async (resolve, reject) => {
+    try {
+      if (!publicKey) {
+        throw new Error("Public key is null or empty");
+      }
+      const paymentData: IPayment[] = [];
+      server
+        .payments()
+        .forAccount(publicKey)
+        .stream({
+          onmessage: (payment: any) => {
+            var asset;
+            if (payment.asset_type === "native") {
+              asset = "lumens";
+            } else {
+              asset = payment.asset_code + ":" + payment.asset_issuer;
+            }
+
+            const paymentEntry: IPayment = {
+              amount: payment.amount,
+              asset: asset,
+              sender: payment.from,
+              receiver: payment.to,
+            };
+
+            if (payment.type !== "create_account") {
+              paymentData.push(paymentEntry);
+              resolve(paymentData);
+            }
+          },
+          onerror: (error: any) => {
+            console.error("Error in payment stream");
+            reject(error);
+          },
+        });
+    } catch (err) {
+      console.error("ERROR!", err);
+      reject(err);
+    }
+  });
+}
+
+export default paymentsHistory;
