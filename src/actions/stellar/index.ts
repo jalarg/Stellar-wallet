ef925b377dbd7baa92baf7296eeedbb4e2fad8e8
jalarg <jlema1990@gmail.com>
2023-10-26 06:11:21 -0300

    Fix capital letter in addminimumbalance

--- src/actions/stellar/index.ts
+++ src/actions/stellar/index.ts
@@ -1,14 +1,14 @@
-import AddMinimumBalance from "./AddMinimumBalance";
+import addMinimumBalance from "./addMinimumBalance";
 import checkBalance from "./checkBalance";
 import sendTransaction from "./sendTransaction";
 import setKeypair from "./setKeypair";
 import getPaymentsHistory from "./getPaymentsHistory";
 import server from "./server";
 import resetKeys from "./resetKeys";
-import albedoLogin from "../wallets/albedoLogin";
+import albedoLogin from "./albedoLogin";
 
 export {
-  AddMinimumBalance,
+  addMinimumBalance,
   checkBalance,
   sendTransaction,
   setKeypair,
